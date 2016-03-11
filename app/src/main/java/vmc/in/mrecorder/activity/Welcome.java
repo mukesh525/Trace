f7765d4ef15078dc2c7f9722fc1ee52683feb8a0
mukesh <mukesh@fe14437a-f66a-4229-b8c6-d36da471a7e5>
2016-03-11 09:20:32 +0000

    10 March

--- app/src/main/java/vmc/in/mrecorder/activity/Welcome.java
+++ app/src/main/java/vmc/in/mrecorder/activity/Welcome.java
@@ -5,23 +5,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
 
 import vmc.in.mrecorder.R;
 
 public class Welcome extends AppCompatActivity {
-
+    private static int SPLASH_TIME_OUT = 3000;
     Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-        btn= (Button) findViewById(R.id.btn_welcome);
-        btn.setOnClickListener(new View.OnClickListener() {
+
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Welcome.this,Login.class));
+            public void run() {
+
+                Intent i = new Intent(Welcome.this, Login.class);
+                startActivity(i);
+                finish();
             }
-        });
+        }, SPLASH_TIME_OUT);
 
     }
 }

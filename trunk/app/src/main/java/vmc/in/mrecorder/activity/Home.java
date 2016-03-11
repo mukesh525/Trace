c58575b15abb161107b8e3205645a4fe800df3b7
mukesh <mukesh@fe14437a-f66a-4229-b8c6-d36da471a7e5>
2016-03-11 05:09:57 +0000

    10 March

--- trunk/app/src/main/java/vmc/in/mrecorder/activity/Home.java
+++ trunk/app/src/main/java/vmc/in/mrecorder/activity/Home.java
@@ -2,9 +2,6 @@ package vmc.in.mrecorder.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -15,6 +12,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import vmc.in.mrecorder.R;
+
 
 public class Home extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {

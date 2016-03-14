200265d93bbab61d51f39985fa4d3c1b7e9341bc
mukesh <mukesh@fe14437a-f66a-4229-b8c6-d36da471a7e5>
2016-03-14 13:35:39 +0000

    14 March

--- app/src/main/java/vmc/in/mrecorder/entity/Util.java
+++ app/src/main/java/vmc/in/mrecorder/entity/Util.java
@@ -24,56 +24,56 @@ import vmc.in.mrecorder.myapplication.CallApplication;
  * Created by gousebabjan on 7/3/16.
  */
 public class Util implements TAG {
-    private static ArrayList<Model> list;
-    private Cursor cursor;
-
-    private String phoneNumber, id;
-    private String time;
-    private String filePath;
-    private String callType;
-    private Model model;
-    private HelperCallRecordings hcr;
-    private Cursor c;
-
-
-    public ArrayList<Model> getCalls(Context context) {
-
-        hcr = new HelperCallRecordings(context);
-        c = hcr.display();
-        list = new ArrayList<Model>();
-        c.moveToFirst();
-        while (c.moveToNext()) {
-            model = new Model();
-            phoneNumber = c.getString(c.getColumnIndex("Number"));
-            time = c.getString(c.getColumnIndex("Time"));
-            callType = c.getString(c.getColumnIndex("CallType"));
-            filePath = c.getString(c.getColumnIndex("FilePath"));
-            id = c.getString(c.getColumnIndex("_id"));
-
-            Log.d("number", "" + phoneNumber);
-            if (id != null) {
-                model.setId(id);
-            }
-            if (phoneNumber != null) {
-                model.setPhoneNumber(phoneNumber);
-            }
-            if (time != null) {
-                model.setTime(time);
-            }
-            if (callType != null) {
-                model.setCallType(callType);
-            }
-            if (filePath != null) {
-                model.setFilePath(filePath);
-                model.setFile(new File(filePath));
-            }
-
-
-            list.add(model);
-        }
-
-        return list;
-    }
+//    private static ArrayList<Model> list;
+//    private Cursor cursor;
+//
+//    private String phoneNumber, id;
+//    private String time;
+//    private String filePath;
+//    private String callType;
+//    private Model model;
+//    private HelperCallRecordings hcr;
+//    private Cursor c;
+//
+//
+//    public ArrayList<Model> getCalls(Context context) {
+//
+//        hcr = new HelperCallRecordings(context);
+//        c = hcr.display();
+//        list = new ArrayList<Model>();
+//        c.moveToFirst();
+//        while (c.moveToNext()) {
+//            model = new Model();
+//            phoneNumber = c.getString(c.getColumnIndex("Number"));
+//            time = c.getString(c.getColumnIndex("Time"));
+//            callType = c.getString(c.getColumnIndex("CallType"));
+//            filePath = c.getString(c.getColumnIndex("FilePath"));
+//            id = c.getString(c.getColumnIndex("_id"));
+//
+//            Log.d("number", "" + phoneNumber);
+//            if (id != null) {
+//                model.setId(id);
+//            }
+//            if (phoneNumber != null) {
+//                model.setPhoneNumber(phoneNumber);
+//            }
+//            if (time != null) {
+//                model.setTime(time);
+//            }
+//            if (callType != null) {
+//                model.setCallType(callType);
+//            }
+//            if (filePath != null) {
+//                model.setFilePath(filePath);
+//                model.setFile(new File(filePath));
+//            }
+//
+//
+//            list.add(model);
+//        }
+//
+//        return list;
+//    }
 
     public static void setRecording(Context context) {
         CallApplication.sp = context.getApplicationContext().getSharedPreferences("com.example.call", Context.MODE_PRIVATE);
@@ -142,4 +142,5 @@ public class Util implements TAG {
     }
 
     ;
+
 }

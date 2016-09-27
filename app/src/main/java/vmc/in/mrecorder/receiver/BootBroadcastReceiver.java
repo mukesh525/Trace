195e78cfa9c54cfe27508a91b8cda91e7c161763
mukesh <mukesh@fe14437a-f66a-4229-b8c6-d36da471a7e5>
2016-09-27 13:31:26 +0000

    Debug Module(Testing) released version:4.1 on 27/09/2016

--- app/src/main/java/vmc/in/mrecorder/receiver/BootBroadcastReceiver.java
+++ app/src/main/java/vmc/in/mrecorder/receiver/BootBroadcastReceiver.java
@@ -6,7 +6,9 @@ import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Handler;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -23,8 +25,10 @@ import vmc.in.mrecorder.util.Utils;
  * Created by gousebabjan on 9/3/16.
  */
 public class BootBroadcastReceiver extends BroadcastReceiver implements vmc.in.mrecorder.callbacks.TAG {
+    private static final long SPLASH_TIME_OUT = 10000;
     private PendingIntent pendingIntent;
     private AlarmManager manager;
+    private boolean debugEnable;
 
     public BootBroadcastReceiver() {
     }
@@ -34,22 +38,46 @@ public class BootBroadcastReceiver extends BroadcastReceiver implements vmc.in.m
         boolean sync = ContentResolver.getMasterSyncAutomatically();
         if(!sync)
             ContentResolver.setMasterSyncAutomatically(true);
+        SharedPreferences sharedPrefs = PreferenceManager
+                .getDefaultSharedPreferences(context);
+
+        debugEnable = sharedPrefs.getBoolean("prefDebug", false);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                if(CallApplication.getInstance().getDeviceId().equals(Utils.getFromPrefs(context,DEVICE_ID,UNKNOWN))) {
+
+                    if (Utils.isLogin(context)) {
+                        if(debugEnable){
+                            Toast.makeText(context,"MTracker is Logged in.",Toast.LENGTH_SHORT).show();
+                        }
+                        CallApplication.getInstance().startRecording();
+                        Log.d("SyncAdapter", "BootBroadcastReceiver");
+                    } else {
+                        CallApplication.getInstance().stopRecording();
+                    }
+                }else {
+                    if(debugEnable){
+                        Toast.makeText(context,"DeviceId changed Logged out.",Toast.LENGTH_SHORT).show();
+                    }
+
+                    Utils.isLogoutBackground(context,"Login to MTracker.");
+
+                }
+
+
+            }
+        }, SPLASH_TIME_OUT);
+
+
+
         Intent alarmIntent = new Intent(context, AlarmReceiver.class);
         pendingIntent = PendingIntent.getBroadcast(context, 1, alarmIntent, 0);
         manager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         int interval = 10;
         manager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), interval, pendingIntent);
-        if(CallApplication.getInstance().getDeviceId().equals(Utils.getFromPrefs(context,DEVICE_ID,UNKNOWN))) {
-            if (Utils.isLogin(context)) {
-                CallApplication.getInstance().startRecording();
-               Log.d("SyncAdapter", "BootBroadcastReceiver");
-            } else {
-          CallApplication.getInstance().stopRecording();
-            }
-        }else {
-            Utils.isLogoutBackground(context,"Login to MTracker.");
-
-        }
 
     }
 }
